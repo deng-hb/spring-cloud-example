8c63ee1f5bcd70b46699bc156da6145a5ac1d351
denghb <i@denghb.com>
2017-02-06 22:48:19 +0800

    Initial commit

---
+++ src/main/java/com/denghb/controller/HelloController.java
@@ -0,0 +1,43 @@
+package com.denghb.controller;
+
+import com.denghb.exception.JsonException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HelloController {
+
+    @Value("${com.denghb.title}")
+    private String title;
+
+    @Value("${com.denghb.random.number}")
+    private String rNumber;
+    @Value("${com.denghb.random.value}")
+    private String rValue;
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String index() {
+        return "Hello World<br/>" + title + "<br/>" + rNumber + "<br/>" + rValue;
+    }
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello World";
+    }
+
+    @RequestMapping("/error1")// "/error"内部保留
+    public String error1() throws Exception {
+        throw new Exception("error1");
+    }
+
+    @RequestMapping("/json-error")
+    @ResponseBody
+    public String jsonError() throws JsonException {
+        throw new JsonException("sada");
+    }
+}
