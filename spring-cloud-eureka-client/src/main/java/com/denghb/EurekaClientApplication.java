5d855d24114a0aa1c9179490e5e2721829140f2a
null <null@null.null>
2020-03-09 23:22:27 +0800

    update

--- spring-cloud-eureka-client/src/main/java/com/denghb/EurekaClientApplication.java
+++ spring-cloud-eureka-client/src/main/java/com/denghb/EurekaClientApplication.java
@@ -1,17 +1,21 @@
 package com.denghb;
 
+
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @SpringBootApplication
 @EnableEurekaClient //表示是eureka的客户端
-@RestController
-@RequestMapping("/client")
+@EnableDiscoveryClient
 public class EurekaClientApplication {
 
     public static void main(String[] args) {
@@ -19,8 +23,24 @@ public class EurekaClientApplication {
         SpringApplication.run(EurekaClientApplication.class);
     }
 
-    @GetMapping("/sayHi")
-    public String sayHi(@RequestParam(value = "name", required = false) String name) {
-        return "Hi, " + name;
+    @RestController
+    class ServiceInstanceRestController {
+
+        @Autowired
+        private DiscoveryClient discoveryClient;
+
+        @Autowired
+        private EurekaClient eurekaClient;
+
+        @RequestMapping("/service-instances/{applicationName}")
+        public List<ServiceInstance> serviceInstancesByApplicationName(
+                @PathVariable String applicationName) {
+            return this.discoveryClient.getInstances(applicationName);
+        }
+
+        @GetMapping("/sayHi")
+        public String sayHi(@RequestParam(value = "name", required = false) String name) {
+            return "Hi, " + name;
+        }
     }
 }
--- spring-cloud-eureka-server/src/main/java/com/denghb/EurekaServerApplication.java
+++ spring-cloud-eureka-server/src/main/java/com/denghb/EurekaServerApplication.java
@@ -3,13 +3,22 @@ package com.denghb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 启动参数
+ * 单机：直接运行
+ *
+ * 两个节点同时运行
+ * 节点1：--spring.profiles.active=node01
+ * 节点2：--spring.profiles.active=node02
+ */
 @SpringBootApplication
-@EnableEurekaServer//标识是eureka服务端
+@EnableEurekaServer
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
