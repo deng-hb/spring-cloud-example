e6e7e65289ca13df7081717945163c3bd5e1e102
null <null@null.null>
2020-03-10 00:41:31 +0800

    update

--- spring-cloud-eureka-client/src/main/java/com/denghb/SayHiController.java
+++ spring-cloud-eureka-client/src/main/java/com/denghb/SayHiController.java
@@ -1,14 +1,18 @@
 package com.denghb;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class SayHiController {
 
     @GetMapping("/sayHi")
     public String sayHi(@RequestParam(value = "name", required = false) String name) {
-        return "Hi, " + name;
+        String say = "Hi, " + name;
+        log.info("say:{}", say);
+        return say;
     }
 }
