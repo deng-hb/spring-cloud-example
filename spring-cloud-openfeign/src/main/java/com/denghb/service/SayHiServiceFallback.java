fdf18375438cac87c5d194cd5e97a1b2dedccecb
null <null@null.null>
2020-03-10 00:59:13 +0800

    update

--- spring-cloud-openfeign/src/main/java/com/denghb/service/SayHiServiceFallback.java
+++ spring-cloud-openfeign/src/main/java/com/denghb/service/SayHiServiceFallback.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Component;
 public class SayHiServiceFallback implements SayHiService {
     @Override
     public String sayHi(String name) {
-        return null;
+        return "error";
     }
 }
