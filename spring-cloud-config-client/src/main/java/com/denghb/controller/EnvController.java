a4a3b9bd43c359e2bcd813b83742516b97ccf1f0
null <null>
2020-03-11 10:35:11 +0800

    update

---
+++ spring-cloud-config-client/src/main/java/com/denghb/controller/EnvController.java
@@ -0,0 +1,17 @@
+package com.denghb.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EnvController {
+
+    @Value("${env:fail}")
+    private String env;
+
+    @GetMapping("/env")
+    public String env() {
+        return env;
+    }
+}
